e6a0d4573be32211d5675b57e178bbfe92e4a331
roberthilwerda <roberthilwerda@gmail.com>
2022-02-27 15:22:15 +0100

    make responsive design

--- frontend/my-app/src/components/Header.js
+++ frontend/my-app/src/components/Header.js
@@ -1,17 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Header.css";
 
 const Header = () => {
+  const [showMenuItems, setShowMenuItems] = useState(false);
+
+  const clickHandler = () => {
+    showMenuItems ? setShowMenuItems(false) : setShowMenuItems(true);
+  };
+
   return (
-    <div className="wrapper">
-      <div className="item">
-        <h4>Home</h4>
+    <div className="header__wrapper">
+      <div className="header__logo-area">
+        <div>
+          <img
+            className={`header__logo`}
+            alt=""
+            src={require("./gtlogo.png")}
+          />
+        </div>
       </div>
-      <div className="item">
-        <h4>Get Trends</h4>
-      </div>
-      <div className="item">
-        <h4>Contact</h4>
+
+      <div onClick={clickHandler} className="header__menu-area">
+        <div className="header__menu-area-hamburger">
+          <h2>{showMenuItems ? "✕" : "☰"}</h2>
+        </div>
+
+        <div
+          className="header__menu-area-items"
+          style={{
+            display: showMenuItems ? "flex" : "none",
+          }}
+        >
+          <div className="header__item">
+            <h4>Home</h4>
+          </div>
+          <div className="header__item">
+            <h4>Get Trends</h4>
+          </div>
+          <div className="header__item">
+            <h4>Contact</h4>
+          </div>
+        </div>
       </div>
     </div>
   );
