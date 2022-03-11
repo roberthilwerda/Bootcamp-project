f49912b08ca0468fba45dba6548746281fb7f310
roberthilwerda <roberthilwerda@gmail.com>
2022-03-11 16:16:24 +0100

    fastapi setup, end of bootcamp day

--- backend/database/crud.py
+++ backend/database/crud.py
@@ -1,11 +1,9 @@
 from sqlalchemy.orm import Session
 
-
 from . import models, schemas
 
-
 def create_genre(db: Session, genre: schemas.Genre):
-    db_item = models.Genre(name="Henk") ## create Genre instance with your data
+    db_item = models.Genre(name=genre.name) ## create Genre instance with your data
     db.add(db_item) 
     db.commit()
     db.refresh(db_item)
--- backend/database/schemas.py
+++ backend/database/schemas.py
@@ -2,7 +2,6 @@ from uuid import UUID
 from pydantic import BaseModel
 
 class Genre(BaseModel):
-    uuid: UUID
     name: str
     class Config:
         orm_mode = True
