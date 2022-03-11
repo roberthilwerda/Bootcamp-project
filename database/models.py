771f175850dd2e7dde23451d5ca787fbcd8b41df
roberthilwerda <roberthilwerda@gmail.com>
2022-03-11 11:12:06 +0100

    new files db

---
+++ database/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+class Artist(Base):
+    __tablename__ = 'Artists'
+    id = Column(int, primary_key=True, index=True)
+    external_urls = Column(String,index = True)
+    followers = Column(Integer, index = True)
+    genres = Column(String, index = True)
+    href = Column(String, index= True)
+    id = Column(String, primary_key= True, index= True)
+    images = Column(String, index= True)
+    name = Column(String, index= True)
+    popularity = Column(Integer, index= True)
+    type = Column(String, index=True )
+    uri = Column(String, index = True)
+
+class Test(Base):
+    __tablename__ = "Test"
+    id = Column(Integer, primary_key=True, index=True)
+
---
+++ database/schemas.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+
+class ArtistBase(BaseModel):
+    title: str
+    description: str | None = None
+
+# class ArtistCreate(ArtistBase):
+#     pass
+
+class Artist(ArtistBase):
+    id: int
+    external_urls: str
+    followers: int
+    genres: int
+    href: int
+    id: int
+    images: int
+    name: int
+    popularity: int 
+    type: int
+    uri: int
+
+    class Config:
+        orm_mode = True
+
