196b6e9ae15aa90129f6b1e362f5780aff3abd49
roberthilwerda <roberthilwerda@gmail.com>
2022-03-11 15:38:21 +0100

    spotify key

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,14 +1,13 @@
 from ast import For
 from turtle import back
-from uuid import UUID
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
-
 from .database import Base
 
 class Artist(Base):
     __tablename__ = 'artist'
-    id = Column(Integer, primary_key=True, index=True)
+    uuid = Column(UUID, primary_key=True, index=True)
     external_urls = Column(String,index = True)
     followers = Column(Integer, index = True)
     genres = Column(String, index = True)
@@ -23,24 +22,24 @@ class Artist(Base):
 
 class Genre(Base):
     __tablename__ = 'genre'
-    id = Column(Integer, primary_key=True, index=True)
+    uuid = Column(UUID, primary_key=True, index=True)
     name = Column(String, index=True)
 
     genre_artist = relationship("GenreArtist", back_populates="genre")
 
 class Song(Base):
     __tablename__ = 'song'
-    id = Column(Integer, primary_key=True, index=True)
+    uuid = Column(UUID, primary_key=True, index=True)
     name = Column(String, index = True)
-    artist_id = Column(Integer, ForeignKey("artist.id"))
+    artist_id = Column(Integer, ForeignKey("artist.uuid"))
 
-    artist = relationship("Artist", back_populates="song")
+    artist = relationship("Artist", back_populates="songs")
 
 class GenreArtist(Base):
     __tablename__ = 'genre_artist'
-    id = Column(Integer, primary_key=True, index=True)
-    genre_id = Column(Integer, ForeignKey("genre.id"))
-    artist_id = Column(Integer, ForeignKey("artist.id"))
+    uuid = Column(UUID, primary_key=True, index=True)
+    genre_id = Column(Integer, ForeignKey("genre.uuid"))
+    artist_id = Column(Integer, ForeignKey("artist.uuid"))
 
     genre = relationship("Genre", back_populates="genre_artist")
     artist = relationship("Artist", back_populates="genre_artist")
