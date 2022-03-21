f0d878ee33f0d0f9d9cd01089821c25e3eeffdf1
roberthilwerda <roberthilwerda@gmail.com>
2022-03-21 12:18:51 +0100

    new function database fill

--- backend/database/utils.py
+++ backend/database/utils.py
@@ -35,6 +35,7 @@ def retrieve_artist_data(artist_name):
 def populate_database(db: Session, date=datetime.today().strftime('%Y-%m-%d')):
     ##extract chart for given date
     chart = extract_chart(date)
+    print(chart.date)
   
     ##for every artist, retrieve spotify data
     for entry in chart.entries:
@@ -56,9 +57,12 @@ def populate_database(db: Session, date=datetime.today().strftime('%Y-%m-%d')):
             crud.create_raw_data(db, new_db_item)
 
 def populate_database_all(db: Session):
-    ## function that returns a list of the past 10 years' months from the day of today
-    ["01-03-2011",  "01-03-2011", ]
-     
+    for year in range(1):
+        for month in range(1,13,1):
+            if month < 10:
+                month = f"0{month}"
+            date = f"{2021+year}-{month}-01"
+            populate_database(db, date)
 
 
        
--- backend/main.py
+++ backend/main.py
@@ -38,10 +38,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("./{genre}"):
-def get_genre_page(genre: str):
-    return genre results
-
 @app.get("/populate_database")
 def populate_database(db: Session = Depends(get_db)):
     return utils.populate_database(db=db)
@@ -50,12 +46,9 @@ def populate_database(db: Session = Depends(get_db)):
 def populate_database_manual(date: str, db: Session = Depends(get_db)):
     return utils.populate_database(db=db, date=date)
 
-<<<<<<< HEAD
-@app.get("./populate_database_all")
+@app.get("/populate_database_all")
 def populate_database_all(db: Session = Depends(get_db)):
     return utils.populate_database_all(db=db)
-=======
->>>>>>> d683b790ffcf4b4967b09ea4ef8ad03ec6cca44d
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8000)
