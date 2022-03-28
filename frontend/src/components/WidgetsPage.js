c15abbd31092220d5f722933e77701b1374ef033
roberthilwerda <roberthilwerda@gmail.com>
2022-03-28 22:53:56 +0200

    add pagination

--- frontend/src/components/WidgetsPage.js
+++ frontend/src/components/WidgetsPage.js
@@ -9,10 +9,12 @@ const WidgetsPage = (props) => {
   const [allData, setAllData] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
   const [selectedGenre, setSelectedGenre] = useState("");
+  const [pagination, setPagination] = useState(0);
 
   const fetchData = useCallback(async () => {
     const response = await fetch("http://localhost:8000/");
     const data = await response.json();
+    setAllData(data);
     setFilteredData(
       data
         .filter((genre) => genre.date === "2021-12-01")
@@ -21,16 +23,11 @@ const WidgetsPage = (props) => {
         })
         .slice(0, 6)
     );
-    setAllData(data);
   }, []);
 
   useEffect(() => {
     fetchData();
   }, [fetchData]);
-
-  function capitalize(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  }
 
   function showComponent(genre) {
     setSelectedGenre(genre);
@@ -51,22 +48,69 @@ const WidgetsPage = (props) => {
 
   const getDataArray = (allData) => {
     let dataArray = [];
-    const genres =  getGenresArray(filteredData);
-    for(const genre of genres){
-
-      dataArray.push(allData.filter(data => data.genre === genre && data.date.includes('2021')))
-    
+    const genres = getGenresArray(filteredData);
+    for (const genre of genres) {
+      dataArray.push(
+        allData.filter(
+          (data) => data.genre === genre && data.date.includes("2021")
+        )
+      );
     }
     // console.log(dataArray)
-    return dataArray
+    return dataArray;
+  };
+
+  const prevPageClickHandler = () => {
+    setPagination(pagination - 1);
+    const newData = allData
+      .filter((genre) => genre.date === "2021-12-01")
+      .sort(function (a, b) {
+        return b.rank_aggregate - a.rank_aggregate;
+      })
+      .slice(6 * (pagination - 1), 6 * (pagination - 1) + 6);
+    setFilteredData(newData);
+  };
+
+  const nextPageClickHandler = () => {
+    setFilteredData(
+      allData
+        .filter((genre) => genre.date === "2021-12-01")
+        .sort(function (a, b) {
+          return b.rank_aggregate - a.rank_aggregate;
+        })
+        .slice(6 * (pagination + 1), 6 * (pagination + 1) + 6)
+    );
+    setPagination(pagination+1)
+  };
+
+  const numOfPages = (allData) => {
+    const length = allData.filter(entry => entry.date === "2021-12-01").length / 6
+    return Math.ceil(length) - 1;
   }
+
+  console.log(numOfPages(allData))
+  console.log(pagination)
 
   const showHomePageContent = () => {
     return (
       <div className="widgetspage__wrapper">
         <div className="widgetspage__col-genres">
           <div style={{ fontSize: 20 }} className="widgetspage__title">
-            <p>Most trending genres</p>
+            <img
+              className="pagination_button"
+              style={{ visibility: pagination === 0 && "hidden" }}
+              alt=""
+              src={require("../img/icon-back.png")}
+              onClick={prevPageClickHandler}
+            />
+            <p>Most popular genres</p>
+            <img
+              style={{ visibility: pagination === numOfPages(allData) && "hidden" }}
+              className="pagination_button"
+              alt=""
+              src={require("../img/icon-forward.png")}
+              onClick={nextPageClickHandler}
+            />
           </div>
 
           {props.showHomePage &&
@@ -74,7 +118,7 @@ const WidgetsPage = (props) => {
               return (
                 <StatsByGenre
                   key={item.id}
-                  ranking={index + 1}
+                  ranking={6 * pagination + index + 1}
                   genre={item.genre}
                   imageUrl={item.image_url}
                   onClickHandler={() => showComponent(item.genre)}
@@ -85,7 +129,31 @@ const WidgetsPage = (props) => {
 
         <div className="widgetspage__col-charts">
           <div style={{ fontSize: 20 }} className="widgetspage__title">
+            <div className="date_input_content">
+              <label>Start date:</label>
+
+              <input
+                className="date_input"
+                type="date"
+                id="start"
+                name="trip-start"
+                min="2012-01-01"
+                max="2021-12-31"
+              />
+            </div>
             <p>Trends</p>
+            <div className="date_input_content">
+              <label>End date:</label>
+
+              <input
+                className="date_input"
+                type="date"
+                id="start"
+                name="trip-start"
+                min="2012-01-01"
+                max="2021-12-31"
+              />
+            </div>
           </div>
           <ChartGenre
             mode={"multiple"}
@@ -113,8 +181,8 @@ const WidgetsPage = (props) => {
     );
   };
 
-
   if (props.showHomePage) {
+
     return showHomePageContent();
   } else {
     return showGenrePageContent(selectedGenre);
